adcccc5dd7c8b9e12772ff31d3372de12e68d50f
pueropstar <majesoung@naver.com>
2023-11-20 22:35:15 +0900

    feat: 드래그 새로고침 기능 추가

--- src/pages/Home/HomeScreen.js
+++ src/pages/Home/HomeScreen.js
@@ -1,22 +1,43 @@
 /** @format */
 
 import * as React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  RefreshControl,
+} from "react-native";
+import { useState, useCallback } from "react";
 import theme from "../../styles/theme";
 import Header from "./Header/Header";
 import HomeContent from "./HomeContent/HomeContent";
 import HomeCategory from "./HomeCategory/HomeCategory";
 
 export default function HomeScreen() {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1000);
+  }, []);
   return (
     <View style={styles.homeScreenView}>
-      <View style={{ flex: 0.5, backgroundColor: theme.psColor }}></View>
+      <View
+        style={{ flex: theme.headerSpace, backgroundColor: theme.psColor }}
+      ></View>
       <View style={styles.homeScreenHeader}>
         <Header />
       </View>
       <View style={{ flex: 7 }}>
-        <ScrollView style={{ flex: 1 }}>
+        <ScrollView
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          style={{ flex: 1 }}
+        >
           <View style={styles.homeScreenCategory}>
             <Text style={styles.homeScreenCategoryText}>카테고리 별로</Text>
             <Text style={styles.homeScreenCategoryText}>확인해 보세요!</Text>
