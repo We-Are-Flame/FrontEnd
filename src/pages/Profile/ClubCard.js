27e4bf09084537f61d1af16a4f38ab1aeeebfea1
마재성 <majesoung@naver.com>
2023-11-22 02:29:27 +0900

    feat: 프로필 모임 진행중
    - 해당 날짜의 모임들이 같이 출력

--- src/pages/Profile/ClubCard.js
+++ src/pages/Profile/ClubCard.js
@@ -3,59 +3,53 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import theme from "../../styles/theme";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import ClubImg from "../../../assets/dummyImage.jpg";
 import { Button, Card } from "react-native-paper";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function ClubCard() {
+export default function ClubCard({ clubData }) {
   return (
-    <View style={styles.clubCardView}>
-      <View style={{ width: "90%", marginTop: 10 }}>
-        <Text style={styles.clubDate}>2023년 9월 30일 (토)</Text>
-        <Card style={styles.manageCard}>
-          <View style={{ flexDirection: "row", ...theme.centerStyle }}>
-            <View style={styles.cardCoverContainer}>
-              <Card.Cover style={styles.cardImageStyle} source={ClubImg} />
-            </View>
-            <View style={styles.cardContentStyle}>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text
-                  style={{
-                    fontSize: 16,
-                    color: "lightgray",
-                    fontWeight: "400",
-                  }}
-                >
-                  금오공과대학교 운동장
-                </Text>
-                <View
-                  style={{
-                    ...styles.clubState,
-                    borderColor: theme.psColor,
-                  }}
-                >
-                  <Text style={{ color: theme.psColor }}>참가중</Text>
-                </View>
-              </View>
-
-              <Text style={{ flex: 1, fontWeight: "600", fontSize: 16 }}>
-                축구 한겜 하실분
-              </Text>
-
-              <Text style={{ flex: 1, fontSize: 14 }}>
-                오후 6:50 ~ 오후 10:50
-              </Text>
+    <Card style={styles.manageCard}>
+      <View style={{ flexDirection: "row", ...theme.centerStyle }}>
+        <View style={styles.cardCoverContainer}>
+          <Card.Cover style={styles.cardImageStyle} source={ClubImg} />
+        </View>
+        <View style={styles.cardContentStyle}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                color: "lightgray",
+                fontWeight: "400",
+              }}
+            >
+              {clubData.location}
+            </Text>
+            <View
+              style={{
+                ...styles.clubState,
+                borderColor: theme.psColor,
+              }}
+            >
+              <Text style={{ color: theme.psColor }}>참가중</Text>
             </View>
           </View>
-        </Card>
+
+          <Text style={{ flex: 1, fontWeight: "600", fontSize: 16 }}>
+            {clubData.clubName}
+          </Text>
+
+          <Text style={{ flex: 1, fontSize: 14 }}>{clubData.time}</Text>
+        </View>
       </View>
-    </View>
+    </Card>
   );
 }
 
