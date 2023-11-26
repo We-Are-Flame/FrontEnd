bae16aac7735fd27325ad2473687ef4f7db84509
마재성 <majesoung@naver.com>
2023-11-26 20:30:57 +0900

    faet: .env 적용

--- src/components/Kakao.js
+++ src/components/Kakao.js
@@ -6,67 +6,24 @@ import { useNavigation } from "@react-navigation/core";
 import { WebView } from "react-native-webview";
 import { REST_API_KEY, REDIRECT_URI } from "@env";
 import axios from "axios";
-
+import { API_URL } from "@env";
 const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 const KaKaoLogin = () => {
   const navigation = useNavigation();
   function KakaoLoginWebView(data) {
-    const exp = "code=";
-    var condition = data.indexOf(exp);
-    if (condition != -1) {
-      var authorize_code = data.substring(condition + exp.length);
-      console.log(authorize_code);
-      requestToken(authorize_code);
-    }
+    // const exp = "code=";
+    console.log(data);
+    navigation.navigate("Login", { screen: "Login" });
+    // var condition = data.indexOf(exp);
+    // if (condition != -1) {
+    //   var authorize_code = data.substring(condition + exp.length);
+    //   console.log(authorize_code);
+    //   requestToken(authorize_code);
+    // }
   }
 
-  const requestToken = async (authorize_code) => {
-    let AccessToken = "none";
-    axios({
-      method: "post",
-      url: "https://kauth.kakao.com/oauth/token",
-      params: {
-        grant_type: "authorization_code",
-        client_id: REST_API_KEY,
-        redirect_uri: REDIRECT_URI,
-        code: authorize_code,
-      },
-    })
-      .then((response) => {
-        AccessToken = response.data.access_token;
-        // console.log(AccessToken);
-        // requestUserInfo(AccessToken);
-        storeaData(AccessToken);
-      })
-      .catch((err) => {
-        console.log("error", error);
-      });
-    navigation.navigate("Login", { screen: "Login" });
-  };
 
-  const requestUserInfo = (AccessToken) => {
-    axios({
-      method: "GET",
-      url: "https://kapi.kakao.com/v2/user/me",
-      headers: {
-        Authorization: `Bearer ${AccessToken}`,
-      },
-    })
-      .then((response) => {
-        let user_email = response.data.kakao_account.email;
-        let user_range = response.data.kakao_account.age_range;
-        let user_gender = response.data.kakao_account.gender;
-
-        console.log(user_email);
-        console.log(user_range);
-        console.log(user_gender);
-        console.log(response.data);
-      })
-      .catch((err) => {
-        console.log("error :", err);
-      });
-  };
   return (
     <View style={Styles.container}>
       <WebView
@@ -74,7 +31,7 @@ const KaKaoLogin = () => {
         originWhitelist={["*"]}
         scalesPageToFit={false}
         source={{
-          uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
+          uri: `${API_URL}/api/login/kakao`,
         }}
         injectedJavaScript={INJECTED_JAVASCRIPT}
         javaScriptEnabled
