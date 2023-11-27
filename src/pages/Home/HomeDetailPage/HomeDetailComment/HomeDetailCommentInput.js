7ae0f160e6ce5e14a92e3974b2c5fe4f121116fb
pueropstar <majesoung@naver.com>
2023-11-27 19:00:23 +0900

    feat: 댓글 작성 기능 추가

--- src/pages/Home/HomeDetailPage/HomeDetailComment/HomeDetailCommentInput.js
+++ src/pages/Home/HomeDetailPage/HomeDetailComment/HomeDetailCommentInput.js
@@ -1,73 +1,107 @@
 /** @format */
 /** @format */
 
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect, useImperativeHandle } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
   ScrollView,
-  TextInput
+  TextInput,
 } from "react-native";
 
-import theme from './../../../../styles/theme';
-import { post_headers } from '../../../../utils/StaticData';
-import axios from 'axios';
+import theme from "./../../../../styles/theme";
+import { post_headers } from "../../../../utils/StaticData";
+import axios from "axios";
 import { API_URL } from "@env";
 
-export default function HomeDetailCommentInput({id}) {
-  const [text,setText] = useState("");
+export default function HomeDetailCommentInput({ id, token }) {
+  const [text, setText] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
 
-  const submitComment = ()=>{
-    axios.post(`${API_URL}/api/meetings/${id}/comments`,
-    {description:text},
-    {headers:post_headers}
-    )
-    .then((res)=>{
-      console.log(res.data);
-      setText("");
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+  const submitComment = () => {
+    axios
+      .post(
+        `${API_URL}/api/meetings/${id}/comments`,
+        { description: text },
+        {
+          headers: {
+            "Content-Type": `application/json`,
+            Authorization: "Bearer " + `${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        setText("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-
-
 
   return (
     <View style={styles.homeDetailCommentInputView}>
-      <TextInput
-        style={styles.inputComment}
-        onChangeText={setText}
-        value={text}
-        placeholder="문의할 내용을 입력해주세요."
-      />
-      <TouchableOpacity style={styles.submitComment} onPress={submitComment}>
-        <Text style={{color:"#ffffff"}}>
-          전송
-        </Text>
-      </TouchableOpacity>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputComment}
+          onChangeText={setText}
+          value={text}
+          placeholder="문의할 내용을 입력해주세요."
+          onFocus={() => {
+            setIsFocused(true);
+          }}
+          onBlur={() => {
+            setIsFocused(false);
+          }}
+        />
+        {isFocused && (
+          <TouchableOpacity
+            style={styles.submitComment}
+            onPress={submitComment}
+          >
+            <Text
+              style={{ color: theme.psColor, fontWeight: "600", fontSize: 18 }}
+            >
+              전송
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  homeDetailCommentInputView:{
-    backgroundColor:"#ffffff",
-    padding:20,
-    flexDirection:"row",
+  homeDetailCommentInputView: {
+    height: 100,
+    backgroundColor: "#ffffff",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "flex-start",
   },
-  inputComment:{
-    backgroundColor:"#eeeeee",
-    borderRadius:20,
-    padding:5,
-    flex:4,
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#eeeeee",
+    borderRadius: 20,
+    padding: 10,
+    height: "60%",
+    width: "100%",
+    position: "relative",
   },
-  submitComment:{
-    padding:10,
-    backgroundColor:theme.psColor,
-    borderRadius:20,
-    ...theme.centerStyle
-  }
+  inputComment: {
+    // backgroundColor: "#eeeeee",
+    // borderRadius: 20,
+    // padding: 5,
+    // flex: 4,
+    // position: "relative",
+    flex: 1,
+  },
+  submitComment: {
+    // backgroundColor: theme.psColor,
+    position: "absolute",
+    right: 20,
+  },
 });
