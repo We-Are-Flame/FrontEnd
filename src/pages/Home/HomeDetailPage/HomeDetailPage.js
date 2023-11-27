6c852de39af76476233d7299e31e9df8fe229914
pueropstar <majesoung@naver.com>
2023-11-27 19:01:12 +0900

    feat: input 창이 키보드 위로 올라오는 기능 추가

--- src/pages/Home/HomeDetailPage/HomeDetailPage.js
+++ src/pages/Home/HomeDetailPage/HomeDetailPage.js
@@ -8,15 +8,19 @@ import {
   TouchableOpacity,
   ScrollView,
   Pressable,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
 } from "react-native";
 import * as Linking from "expo-linking";
 import { Image } from "expo-image";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 import { Ionicons, AntDesign, MaterialIcons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import Spinner from "../../../../assets/loading_spinner.svg";
 import theme from "../../../styles/theme";
-
 import HomeDetailComment from "./HomeDetailComment/HomeDetailComment";
 import HomeDetailCommentInput from "./HomeDetailComment/HomeDetailCommentInput";
 import { headers } from "./../../../utils/StaticData";
@@ -31,6 +35,7 @@ export default function HomeDetailPage({ route }) {
   const [imageSource, setImageSource] = useState("");
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
+  const [userToken, setUserToken] = useState("");
 
   const navigation = useNavigation();
   const stateId = route.params.id;
@@ -114,206 +119,230 @@ export default function HomeDetailPage({ route }) {
     }
   }, [detailData]);
 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem("userAccessToken");
+        if (value !== null) {
+          setUserToken(value);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, []);
+
   return Object.keys(detailData).length !== 0 ? (
-    <View style={styles.homeDetailPageView}>
-      <View style={styles.homeDetailContentView}>
-        <ScrollView style={styles.homeDetailScrollView}>
-          <View style={styles.homeDetailTitle}>
-            <View style={{ flexDirection: "row" }}>
-              {imageSource ? (
-                <Image
-                  source={imageSource}
-                  style={{
-                    width: 24,
-                    height: 24,
-                    marginTop: 3,
-                    marginRight: 5,
-                  }} // 예시 크기, 원하는 대로 조절
-                  resizeMode="cover" // 또는 "contain", "stretch" 등
-                />
-              ) : (
-                <Ionicons name="person" size={24} color="black" />
-              )}
-              <View>
-                <View style={{ flexDirection: "row" }}>
-                  <Text style={{ fontWeight: "bold", fontSize: 10 }}>
-                    {detailData.info.title}&nbsp;&nbsp;&nbsp;
-                  </Text>
-                  <Text
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={60}
+    >
+      <View style={styles.homeDetailPageView}>
+        <View style={styles.homeDetailContentView}>
+          <ScrollView style={styles.homeDetailScrollView}>
+            <View style={styles.homeDetailTitle}>
+              <View style={{ flexDirection: "row" }}>
+                {imageSource ? (
+                  <Image
+                    source={imageSource}
                     style={{
-                      color: "#aaaaaa",
-                      fontWeight: "bold",
-                      fontSize: 10,
-                    }}
-                  >
-                    {detailData.info.category || "스포츠"}
+                      width: 24,
+                      height: 24,
+                      marginTop: 3,
+                      marginRight: 5,
+                    }} // 예시 크기, 원하는 대로 조절
+                    resizeMode="cover" // 또는 "contain", "stretch" 등
+                  />
+                ) : (
+                  <Ionicons name="person" size={24} color="black" />
+                )}
+                <View>
+                  <View style={{ flexDirection: "row" }}>
+                    <Text style={{ fontWeight: "bold", fontSize: 10 }}>
+                      {detailData.info.title}&nbsp;&nbsp;&nbsp;
+                    </Text>
+                    <Text
+                      style={{
+                        color: "#aaaaaa",
+                        fontWeight: "bold",
+                        fontSize: 10,
+                      }}
+                    >
+                      {detailData.info.category || "스포츠"}
+                    </Text>
+                  </View>
+                  <Text style={{ fontWeight: "bold" }}>
+                    {detailData.host.name}
                   </Text>
                 </View>
-                <Text style={{ fontWeight: "bold" }}>
-                  {detailData.host.name}
+              </View>
+              <View></View>
+              <View>
+                {detailData.status.is_owner ? (
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("ClubManagePage")}
+                  >
+                    <Text style={styles.homeDetailAppManageBtn}>신청관리</Text>
+                  </TouchableOpacity>
+                ) : (
+                  ""
+                )}
+              </View>
+              <View>
+                <AntDesign name="ellipsis1" size={30} color="black" />
+              </View>
+            </View>
+
+            <View style={styles.homeDetailContent}>
+              <Text style={{ fontWeight: "bold", fontSize: 22 }}>
+                {detailData.info.title}
+              </Text>
+              <Text style={{ marginTop: 10 }}>
+                {detailData.info.description}
+              </Text>
+              <Text style={{ color: theme.psColor }}>{hashtagString}</Text>
+
+              {/* 여기에 종료된게임, 참가신청, 참가취소 버튼 추가 */}
+              {detailData.status.is_expire ? (
+                <TouchableOpacity style={styles.homeDetailStateBtnGray}>
+                  <Text style={styles.homeDetailStateTextGray}>
+                    종료된 게임
+                  </Text>
+                </TouchableOpacity>
+              ) : detailData.status.participate_status === "NONE" ? (
+                <TouchableOpacity style={styles.homeDetailStateBtnBlue}>
+                  <Text style={styles.homeDetailStateTextBlue}>참가 신청</Text>
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity style={styles.homeDetailStateBtnRed}>
+                  <Text style={styles.homeDetailStateTextRed}>참가 취소</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+
+            <View style={styles.homeDetailSpace} />
+
+            <View style={styles.homeDetailInformation}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ flexDirection: "row", flex: 4 }}>
+                  <AntDesign name="calendar" size={24} color="black" />
+                  <Text style={styles.informationFormmat}>날짜</Text>
+                </View>
+                <Text style={{ ...styles.informationFormmat, flex: 6 }}>
+                  {dateStirng}
                 </Text>
               </View>
             </View>
-            <View></View>
-            <View>
-              {detailData.status.is_owner ? (
-                <TouchableOpacity
-                  onPress={() => navigation.navigate("ClubManagePage")}
-                >
-                  <Text style={styles.homeDetailAppManageBtn}>신청관리</Text>
-                </TouchableOpacity>
-              ) : (
-                ""
-              )}
-            </View>
-            <View>
-              <AntDesign name="ellipsis1" size={30} color="black" />
-            </View>
-          </View>
 
-          <View style={styles.homeDetailContent}>
-            <Text style={{ fontWeight: "bold", fontSize: 22 }}>
-              {detailData.info.title}
-            </Text>
-            <Text style={{ marginTop: 10 }}>{detailData.info.description}</Text>
-            <Text style={{ color: theme.psColor }}>{hashtagString}</Text>
-
-            {/* 여기에 종료된게임, 참가신청, 참가취소 버튼 추가 */}
-            {detailData.status.is_expire ? (
-              <TouchableOpacity style={styles.homeDetailStateBtnGray}>
-                <Text style={styles.homeDetailStateTextGray}>종료된 게임</Text>
-              </TouchableOpacity>
-            ) : detailData.status.participate_status === "NONE" ? (
-              <TouchableOpacity style={styles.homeDetailStateBtnBlue}>
-                <Text style={styles.homeDetailStateTextBlue}>참가 신청</Text>
-              </TouchableOpacity>
-            ) : (
-              <TouchableOpacity style={styles.homeDetailStateBtnRed}>
-                <Text style={styles.homeDetailStateTextRed}>참가 취소</Text>
-              </TouchableOpacity>
-            )}
-          </View>
-
-          <View style={styles.homeDetailSpace} />
-
-          <View style={styles.homeDetailInformation}>
-            <View style={{ flexDirection: "row" }}>
-              <View style={{ flexDirection: "row", flex: 4 }}>
-                <AntDesign name="calendar" size={24} color="black" />
-                <Text style={styles.informationFormmat}>날짜</Text>
+            <View style={styles.homeDetailInformation}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ flexDirection: "row", flex: 4 }}>
+                  <AntDesign name="clockcircle" size={24} color="black" />
+                  <Text style={styles.informationFormmat}>시간</Text>
+                </View>
+                <Text style={{ ...styles.informationFormmat, flex: 6 }}>
+                  {timeRangeString}
+                </Text>
+                <Text></Text>
               </View>
-              <Text style={{ ...styles.informationFormmat, flex: 6 }}>
-                {dateStirng}
-              </Text>
             </View>
-          </View>
 
-          <View style={styles.homeDetailInformation}>
-            <View style={{ flexDirection: "row" }}>
-              <View style={{ flexDirection: "row", flex: 4 }}>
-                <AntDesign name="clockcircle" size={24} color="black" />
-                <Text style={styles.informationFormmat}>시간</Text>
+            <View style={styles.homeDetailInformation}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ flexDirection: "row", flex: 4 }}>
+                  <Ionicons name="people" size={24} color="black" />
+                  <Text style={styles.informationFormmat}>모집인원</Text>
+                </View>
+                <Text style={{ ...styles.informationFormmat, flex: 6 }}>
+                  {detailData.info.current_participants} /{" "}
+                  {detailData.info.max_participants}
+                </Text>
+                <Text></Text>
               </View>
-              <Text style={{ ...styles.informationFormmat, flex: 6 }}>
-                {timeRangeString}
-              </Text>
-              <Text></Text>
             </View>
-          </View>
 
-          <View style={styles.homeDetailInformation}>
-            <View style={{ flexDirection: "row" }}>
-              <View style={{ flexDirection: "row", flex: 4 }}>
-                <Ionicons name="people" size={24} color="black" />
-                <Text style={styles.informationFormmat}>모집인원</Text>
-              </View>
-              <Text style={{ ...styles.informationFormmat, flex: 6 }}>
-                {detailData.info.current_participants} /{" "}
-                {detailData.info.max_participants}
-              </Text>
-              <Text></Text>
-            </View>
-          </View>
-
-          <View style={styles.homeDetailInformation}>
-            <View style={{ flexDirection: "row" }}>
-              <View style={{ flexDirection: "row", flex: 4 }}>
-                <MaterialIcons name="place" size={24} color="black" />
-                <Text style={styles.informationFormmat}>위치</Text>
-              </View>
-              <Text
-                style={{
-                  ...styles.informationFormmat,
-                  flex: 6,
-                }}
-              >
-                {`${detailData.location.location} ${detailData.location.detail_location}`}
-              </Text>
-              <Text></Text>
-            </View>
-          </View>
-
-          <View style={styles.homeDetailMap}>
-            <GoogleMap />
-          </View>
-
-          <View
-            style={{
-              backgroundColor: "white",
-              padding: 10,
-            }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                ...theme.centerStyle,
-                backgroundColor: "#f5f5f5",
-                height: 60,
-                paddingVertical: 5,
-                paddingHorizontal: 15,
-                borderRadius: 5,
-              }}
-            >
-              <Text
-                style={{ flex: 8, color: theme.psColor, fontWeight: "bold" }}
-              >
-                {detailData.location.location}
-              </Text>
-              <View
-                style={{
-                  flex: 2,
-                  height: 35,
-                  borderRadius: 20,
-                  backgroundColor: "#ffffff",
-                  ...theme.centerStyle,
-                }}
-              >
-                <Pressable
-                  onPress={() => {
-                    Linking.openURL(
-                      `http://maps.google.com/?q=${detailData.location.location}`
-                    );
+            <View style={styles.homeDetailInformation}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ flexDirection: "row", flex: 4 }}>
+                  <MaterialIcons name="place" size={24} color="black" />
+                  <Text style={styles.informationFormmat}>위치</Text>
+                </View>
+                <Text
+                  style={{
+                    ...styles.informationFormmat,
+                    flex: 6,
                   }}
                 >
-                  <Text
-                    style={{
-                      color: theme.psColor,
-                    }}
-                  >
-                    길찾기
-                  </Text>
-                </Pressable>
+                  {`${detailData.location.location} ${detailData.location.detail_location}`}
+                </Text>
+                <Text></Text>
               </View>
             </View>
-          </View>
 
-          <View style={styles.homeDetailSpace} />
+            <View style={styles.homeDetailMap}>
+              <GoogleMap />
+            </View>
 
-          <HomeDetailComment id={stateId} />
-        </ScrollView>
+            <View
+              style={{
+                backgroundColor: "white",
+                padding: 10,
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  ...theme.centerStyle,
+                  backgroundColor: "#f5f5f5",
+                  height: 60,
+                  paddingVertical: 5,
+                  paddingHorizontal: 15,
+                  borderRadius: 5,
+                }}
+              >
+                <Text
+                  style={{ flex: 8, color: theme.psColor, fontWeight: "bold" }}
+                >
+                  {detailData.location.location}
+                </Text>
+                <View
+                  style={{
+                    flex: 2,
+                    height: 35,
+                    borderRadius: 20,
+                    backgroundColor: "#ffffff",
+                    ...theme.centerStyle,
+                  }}
+                >
+                  <Pressable
+                    onPress={() => {
+                      Linking.openURL(
+                        `http://maps.google.com/?q=${detailData.location.location}`
+                      );
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: theme.psColor,
+                      }}
+                    >
+                      길찾기
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+            </View>
+
+            <View style={styles.homeDetailSpace} />
+
+            <HomeDetailComment id={stateId} />
+          </ScrollView>
+        </View>
+        <HomeDetailCommentInput id={stateId} token={userToken} />
       </View>
-      <HomeDetailCommentInput id={stateId} />
-    </View>
+    </KeyboardAvoidingView>
   ) : (
     <Image
       source={Spinner}
