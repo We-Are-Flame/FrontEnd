0fce13272a2ae69b4070d7c89a600f9090955fa0
마재성 <majesoung@naver.com>
2023-11-28 03:10:36 +0900

    feat: 렌더링 후 토큰 유효성을 검사하도록 설정

--- src/pages/Home/HomeScreen.js
+++ src/pages/Home/HomeScreen.js
@@ -11,24 +11,25 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { FAB } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 import theme from "../../styles/theme";
 import Header from "../../components/Header";
 import HomeContent from "./HomeContent/HomeContent";
 import HomeCategory from "./HomeCategory/HomeCategory";
 import Dropdown from "../../components/Dropdown";
-
-import { Ionicons } from "@expo/vector-icons";
-
 import { sort } from "../../utils/StaticData";
-
+import { API_URL } from "@env";
 export default function HomeScreen() {
   const [refreshing, setRefreshing] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedSort, setSelectedSort] = useState(sort[0]);
+  const [isLogin, setIsLogin] = useState(false);
   const navigation = useNavigation();
 
   const onRefresh = useCallback(() => {
@@ -38,13 +39,39 @@ export default function HomeScreen() {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await AsyncStorage.getItem("userAccessToken");
+        if (token !== null) {
+          const response = await axios.get(`${API_URL}/api/user/notification`, {
+            headers: {
+              "Content-Type": `application/json`,
+              Authorization: "Bearer " + `${token}`,
+            },
+          });
+
+          if (response.status === 200) {
+            console.log("Success:", response.data);
+            setIsLogin(true);
+          } else {
+            console.log("Unexpected status code:", response.status);
+          }
+        }
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.homeScreenView}>
       <View
         style={{ flex: theme.headerSpace, backgroundColor: theme.psColor }}
       ></View>
 
-      <Header />
+      <Header isLogin={isLogin}/>
 
       <View style={{ flex: 7 }}>
         <ScrollView
