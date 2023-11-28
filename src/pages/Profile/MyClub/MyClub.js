5fc7311fbf95ef11035a346c9abb1ab45693efe2
pueropstar <majesoung@naver.com>
2023-11-28 20:13:09 +0900

    feat: 내가 생성한 모임 추가

--- src/pages/Profile/MyClub/MyClub.js
+++ src/pages/Profile/MyClub/MyClub.js
@@ -15,27 +15,36 @@ import {
 } from "react-native";
 import ClubCard from "../ClubCard/ClubCard";
 import { API_URL } from "@env";
+import { render } from "react-dom";
 
-const FirstRoute = () => (
+const FirstRoute = ({ myClubData }) => (
   <ScrollView contentContainerStyle={{ flex: 1 }}>
-    <View style={styles.clubCardView}>
-      <View style={{ width: "90%" }}>
-        {myclubData.map((day, index) => (
-          <Fragment key={index}>
-            <Text style={{ ...styles.clubDate, marginTop: 20 }}>
-              {day.date}
-            </Text>
-            {day.clubs.map((club, clubIndex) => (
-              <ClubCard key={clubIndex} clubData={club} />
-            ))}
-          </Fragment>
-        ))}
+    {myClubData && myClubData.count === 0 ? (
+      <View style={styles.clubCardView}>
+        <View style={{ width: "90%" }}>
+          {myClubData.map((day, index) => (
+            <Fragment key={index}>
+              <Text style={{ ...styles.clubDate, marginTop: 20 }}>
+                {day.date}
+              </Text>
+              {day.clubs.map((club, clubIndex) => (
+                <ClubCard key={clubIndex} clubData={club} />
+              ))}
+            </Fragment>
+          ))}
+        </View>
       </View>
-    </View>
+    ) : (
+      <View style={{ flex: 1, ...theme.centerStyle }}>
+        <Text style={{ fontSize: 16, color: "lightgray" }}>
+          생성한 모임이 없습니다.
+        </Text>
+      </View>
+    )}
   </ScrollView>
 );
 
-const SecondRoute = () => (
+const SecondRoute = ({ meetings }) => (
   <ScrollView contentContainerStyle={{ flex: 1 }}>
     <View style={{ flex: 1, ...theme.centerStyle }}>
       <Text style={{ fontSize: 16, color: "lightgray" }}>
@@ -59,34 +68,19 @@ const renderTabBar = (props) => (
   />
 );
 
-export default function MyClub({ userToken }) {
+export default function MyClub({ myClubData }) {
   const [index, setIndex] = useState(0);
   const [routes] = useState([
     { key: "first", title: "내가 생성한 모임" },
     { key: "second", title: "내가 참여한 모임" },
   ]);
-  // useEffect(() => {
-  //   axios
-  //     .get(`${API_URL}/api/meetings/my`, {
-  //       headers: {
-  //         "Content-Type": `application/json`,
-  //         Authorization: "Bearer " + `${userToken}`,
-  //       },
-  //     })
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
 
   return (
     <View style={styles.myClubView}>
       <TabView
         renderTabBar={renderTabBar}
         navigationState={{ index, routes }}
-        renderScene={renderScene}
+        renderScene={(props) => renderScene({ ...props, myClubData })}
         onIndexChange={setIndex}
       />
     </View>
