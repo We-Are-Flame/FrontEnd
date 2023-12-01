d3c771797a4156dabd6a6b13f931d045be36a322
마재성 <majesoung@naver.com>
2023-12-02 02:33:40 +0900

    feat: 날짜별 모임 그룹화 추가
    - 내가 만든 모임이 날짜별로
    그룹화

--- src/pages/Profile/MyClub/MyClub.js
+++ src/pages/Profile/MyClub/MyClub.js
@@ -2,10 +2,8 @@
 
 import * as React from "react";
 import theme from "../../../styles/theme";
-import axios from "axios";
 import { useState, Fragment, useEffect } from "react";
 import { TabView, SceneMap, TabBar } from "react-native-tab-view";
-import { myclubData } from "../../../utils/StaticData";
 import {
   View,
   Text,
@@ -14,28 +12,52 @@ import {
   ScrollView,
 } from "react-native";
 import ClubCard from "../ClubCard/ClubCard";
-import { API_URL } from "@env";
-import { render } from "react-dom";
+
+const convertDate = (inputDate) => {
+  const dateObj = new Date(inputDate);
+
+  const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+  const returnDay = daysOfWeek[dateObj.getDay()];
+
+  return returnDay;
+};
 
 const FirstRoute = ({ myClubData }) => {
+  const meetings = myClubData.content || [];
+
+  const groupMeetingsByDate = (meetings) => {
+    return meetings.reduce((grouped, meeting) => {
+      const date = meeting.time.start_time.split("T")[0];
+      if (!grouped[date]) {
+        grouped[date] = [];
+      }
+      grouped[date].push(meeting);
+      return grouped;
+    }, {});
+  };
+
+  const groupedMeetings = groupMeetingsByDate(meetings);
+
   return (
     <ScrollView style={{ flex: 1 }}>
-      {Object.keys(myClubData).length !== 0 && myClubData.count !== 0 ? (
+      {Object.keys(groupedMeetings).length !== 0 ? (
         <View style={styles.clubCardView}>
           <View style={{ width: "90%" }}>
-            {myClubData.content.map((meeting, index) => (
-              <Fragment key={index}>
+            {Object.entries(groupedMeetings).map(([date, meetings]) => (
+              <Fragment key={date}>
                 <Text
                   style={{
                     ...styles.clubDate,
                     marginTop: 20,
-                    height: theme.screenHeight / 20,
+                    height: theme.screenHeight / 25,
                   }}
                 >
-                  {meeting.time.start_time}
+                  {`${date} (${convertDate(meetings[0].time.start_time)})`}
                 </Text>
                 <View style={{ flex: 1 }}>
-                  <ClubCard key={index} clubData={meeting} />
+                  {meetings.map((meeting, index) => (
+                    <ClubCard key={index} clubData={meeting} />
+                  ))}
                 </View>
               </Fragment>
             ))}
@@ -61,11 +83,6 @@ const SecondRoute = ({ meetings }) => (
     </View>
   </ScrollView>
 );
-
-// const renderScene = SceneMap({
-//   first: FirstRoute,
-//   second: SecondRoute,
-// });
 
 const renderTabBar = (props) => (
   <TabBar
