4cb933d0c1e3570a01d117e97147e4a16be061dd
마재성 <majesoung@naver.com>
2023-12-03 02:03:36 +0900

    style: 신청 관리 UI 수정중

--- src/pages/Club/ClubManagePage.js
+++ src/pages/Club/ClubManagePage.js
@@ -3,24 +3,49 @@
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { useState } from "react";
 
-import Header from "../../components/Header";
 import theme from "../../styles/theme";
-import ManageContentContainer from "./ManageContentContainer/ManageContentContainer";
+import { Ionicons } from "@expo/vector-icons";
+
+import ManageCard from "./ManageContentItem/ManageCard/ManageCard";
 export default function ClubManagePage() {
+  const [isSelect, setIsSelect] = useState();
   return (
     <View style={styles.managePageView}>
-      <View
-        style={{ flex: theme.headerSpace, backgroundColor: theme.psColor }}
-      ></View>
-      <View style={styles.managePageHeader}>
-        <Header />
-      </View>
       <View style={styles.managePageContent}>
-        <ScrollView style={{ flex: 1 }}>
-          <ManageContentContainer />
-        </ScrollView>
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              marginBottom: 10,
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+          >
+            <View style={styles.headerLeft}>
+              {isSelect ? (
+                <Ionicons name="checkmark-circle" size={24} color="black" />
+              ) : (
+                <Ionicons name="ellipse-outline" size={24} color="black" />
+              )}
+              <Text
+                style={{
+                  ...styles.headerText,
+                  marginLeft: theme.screenWidth / 60,
+                }}
+              >
+                전체 4개
+              </Text>
+            </View>
+            <View style={styles.headerRight}>
+              <Text style={styles.headerText}>선택 거절</Text>
+            </View>
+          </View>
+
+          <ScrollView style={{ flex: 1 }}>
+            <ManageCard />
+          </ScrollView>
+        </View>
       </View>
-      <View style={{ height: 100 }}></View>
     </View>
   );
 }
@@ -29,11 +54,21 @@ const styles = StyleSheet.create({
   managePageView: {
     flex: 1,
     backgroundColor: "white",
+    padding: 20,
+  },
+  managePageContent: {
+    flex: 1,
+    // backgroundColor: "red",
   },
   managePageHeader: {
     flex: 1,
   },
-  managePageContent: {
-    flex: 7,
+  headerLeft: { flex: 8, flexDirection: "row", alignItems: "center" },
+  headerRight: {
+    flex: 2,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
   },
+  headerText: { fontSize: 16, fontWeight: "600", textAlignVertical: "bottom" },
 });
--- src/pages/Club/ManageContentItem/ManageCard/ManageCard.js
+++ src/pages/Club/ManageContentItem/ManageCard/ManageCard.js
@@ -1,71 +1,149 @@
+// /** @format */
+
+// import * as React from "react";
+// import { useState, useEffect } from "react";
+// import { View, Text, StyleSheet } from "react-native";
+// import LionImg from "../../../../../assets/lion.webp";
+// import { Button, Card } from "react-native-paper";
+// import theme from "../../../../styles/theme";
+// export default function ManageCard({ state, flag, onAccept, onReject }) {
+//   return (
+//     <View style={{ ...styles.manageCard, width: flag ? "98%" : "100%" }}>
+//       {state ? (
+//         <Card style={{}}>
+//           <View style={{ flexDirection: "row" }}>
+//             <View style={styles.cardCoverContainer}>
+//               <Card.Cover style={styles.cardImageStyle} source={LionImg} />
+//             </View>
+//             <View style={styles.cardContentStyle}>
+//               <Text style={{ fontSize: 20, fontWeight: "600" }}>
+//                 {state.nickname}
+//               </Text>
+//               <View style={{ flex: 1, flexDirection: "row" }}>
+//                 <View style={{ flex: 2, justifyContent: "center" }}>
+//                   <Text>온도</Text>
+//                   <Text>{state.temper}º</Text>
+//                 </View>
+//                 <View style={{ flex: 8 }}>
+//                   <Card.Actions>
+//                     <Button
+//                       style={{ borderWidth: 0 }}
+//                       buttonColor={theme.psColor}
+//                       textColor="white"
+//                       onPress={() => onAccept(state.id)}
+//                     >
+//                       수락
+//                     </Button>
+//                     <Button
+//                       style={{ borderWidth: 0 }}
+//                       buttonColor="#fa9797"
+//                       textColor="white"
+//                       onPress={() => onReject(state.id)}
+//                     >
+//                       거절
+//                     </Button>
+//                   </Card.Actions>
+//                 </View>
+//               </View>
+//             </View>
+//           </View>
+//         </Card>
+//       ) : null}
+//     </View>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   manageCard: {
+//     flex: 2,
+//     marginTop: 5,
+//   },
+//   cardCoverContainer: {
+//     ...theme.centerStyle,
+//     flex: 2.7,
+//     padding: 5,
+//   },
+//   cardImageStyle: { flexGrow: 0, width: "100%", height: "100%" },
+//   cardContentStyle: {
+//     flex: 7.3,
+//     paddingVertical: 12,
+//     paddingHorizontal: 5,
+//   },
+// });
+
 /** @format */
 
 import * as React from "react";
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import LionImg from "../../../../../assets/lion.webp";
-import { Button, Card } from "react-native-paper";
+import { Button } from "react-native-paper";
 import theme from "../../../../styles/theme";
-export default function ManageCard({ state, flag, onAccept, onReject }) {
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+export default function ManageCard() {
+  const [isSelect, setIsSelect] = useState(false);
   return (
-    <View style={{ ...styles.manageCard, width: flag ? "98%" : "100%" }}>
-      {state ? (
-        <Card style={{}}>
-          <View style={{ flexDirection: "row" }}>
-            <View style={styles.cardCoverContainer}>
-              <Card.Cover style={styles.cardImageStyle} source={LionImg} />
-            </View>
-            <View style={styles.cardContentStyle}>
-              <Text style={{ fontSize: 20, fontWeight: "600" }}>
-                {state.nickname}
-              </Text>
-              <View style={{ flex: 1, flexDirection: "row" }}>
-                <View style={{ flex: 2, justifyContent: "center" }}>
-                  <Text>온도</Text>
-                  <Text>{state.temper}º</Text>
-                </View>
-                <View style={{ flex: 8 }}>
-                  <Card.Actions>
-                    <Button
-                      style={{ borderWidth: 0 }}
-                      buttonColor={theme.psColor}
-                      textColor="white"
-                      onPress={() => onAccept(state.id)}
-                    >
-                      수락
-                    </Button>
-                    <Button
-                      style={{ borderWidth: 0 }}
-                      buttonColor="#fa9797"
-                      textColor="white"
-                      onPress={() => onReject(state.id)}
-                    >
-                      거절
-                    </Button>
-                  </Card.Actions>
-                </View>
-              </View>
-            </View>
+    <View style={styles.manageCardView}>
+      <View style={styles.manageCard}>
+        <View style={{ justifyContent: "center", flex: 0.4 }}>
+          {isSelect ? (
+            <Ionicons name="checkmark-circle" size={24} color="black" />
+          ) : (
+            <Ionicons name="ellipse-outline" size={24} color="black" />
+          )}
+        </View>
+        <View style={{ flex: 1 }}>
+          <Image style={styles.image} source={LionImg} contentFit="cover" />
+        </View>
+        <View style={{ justifyContent: "center", flex: 1 }}>
+          <Text style={{ fontWeight: "600" }}>우움</Text>
+          <Text>온도 990 º</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <View style={{ flex: 1 }}>
+            <TouchableOpacity>
+              <Button
+                style={{ borderWidth: 0 }}
+                buttonColor={theme.psColor}
+                textColor="white"
+              >
+                수락
+              </Button>
+            </TouchableOpacity>
           </View>
-        </Card>
-      ) : null}
+          <View style={{ flex: 1 }}>
+            <TouchableOpacity>
+              <Button
+                style={{ borderWidth: 0 }}
+                buttonColor="#fa9797"
+                textColor="white"
+              >
+                거절
+              </Button>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  manageCardView: { height: theme.screenHeight / 10, borderWidth: 1 },
   manageCard: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  image: {
+    borderWidth: 1,
+    borderColor: theme.profileBorderColor,
+    width: 60,
+    height: 60,
+    borderRadius: theme.screenWidth / 6,
+  },
+  buttonContainer: {
     flex: 2,
-    marginTop: 5,
-  },
-  cardCoverContainer: {
-    ...theme.centerStyle,
-    flex: 2.7,
-    padding: 5,
-  },
-  cardImageStyle: { flexGrow: 0, width: "100%", height: "100%" },
-  cardContentStyle: {
-    flex: 7.3,
-    paddingVertical: 12,
-    paddingHorizontal: 5,
+    flexDirection: "row",
+    justifyContent: "flex-end",
   },
 });
