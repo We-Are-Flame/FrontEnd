cfedf843477183ee2a5990a6f2bbe17c6ed87f70
마재성 <majesoung@naver.com>
2023-12-03 23:49:46 +0900

    feat: 체크 박스 구현중

--- src/pages/Club/ClubManagePage.js
+++ src/pages/Club/ClubManagePage.js
@@ -1,14 +1,36 @@
 /** @format */
 
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import { useState } from "react";
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+import { useEffect, useState } from "react";
+import { Button } from "react-native-paper";
+import { participateList } from "../../utils/StaticData";
 import theme from "../../styles/theme";
 import { Ionicons } from "@expo/vector-icons";
-
 import ManageCard from "./ManageContentItem/ManageCard/ManageCard";
 export default function ClubManagePage() {
   const [isSelect, setIsSelect] = useState();
+
+  const [checkItems, setCheckItems] = useState(new Set()); // 체크 된 애들을 담는 집합
+  const [isAllChecked, setIsAllChecked] = useState(false);
+
+  const checkItemsHandler = (id, isChecked) => {
+    const newCheckItems = new Set(checkItems);
+    if (isChecked) {
+      newCheckItems.add(id);
+    } else {
+      newCheckItems.delete(id);
+    }
+    setCheckItems(newCheckItems);
+  };
+
+  const allCheckHandler = () => {};
   return (
     <View style={styles.managePageView}>
       <View style={styles.managePageContent}>
@@ -22,11 +44,17 @@ export default function ClubManagePage() {
             }}
           >
             <View style={styles.headerLeft}>
-              {isSelect ? (
-                <Ionicons name="checkmark-circle" size={24} color="black" />
-              ) : (
-                <Ionicons name="ellipse-outline" size={24} color="black" />
-              )}
+              <Pressable
+                onPress={() => {
+                  setIsSelect(!isSelect);
+                }}
+              >
+                {isSelect ? (
+                  <Ionicons name="checkmark-circle" size={24} color="black" />
+                ) : (
+                  <Ionicons name="ellipse-outline" size={24} color="black" />
+                )}
+              </Pressable>
               <Text
                 style={{
                   ...styles.headerText,
@@ -42,8 +70,34 @@ export default function ClubManagePage() {
           </View>
 
           <ScrollView style={{ flex: 1 }}>
-            <ManageCard />
+            {participateList.content.map((data, index) => {
+              return (
+                <ManageCard
+                  data={data}
+                  key={index}
+                  id={`id` + index}
+                  checkItemsHandler={checkItemsHandler}
+                />
+              );
+            })}
           </ScrollView>
+          <View style={{ flex: 0.2 }}>
+            <TouchableOpacity>
+              <Button
+                style={{
+                  borderWidth: 0,
+                  height: theme.screenHeight / 15,
+                  justifyContent: "center",
+                  borderRadius: 5,
+                }}
+                labelStyle={{ fontSize: theme.screenWidth / 25 }}
+                buttonColor={theme.psColor}
+                textColor="white"
+              >
+                {checkItems.size}명 수락하기
+              </Button>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
--- src/pages/Club/ManageContentItem/ManageCard/ManageCard.js
+++ src/pages/Club/ManageContentItem/ManageCard/ManageCard.js
@@ -80,17 +80,28 @@ import { Button } from "react-native-paper";
 import theme from "../../../../styles/theme";
 import { Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
-export default function ManageCard() {
-  const [isSelect, setIsSelect] = useState(false);
+import { Pressable } from "react-native";
+export default function ManageCard({ checkItemsHandler, id }) {
+  const [checked, setChecked] = useState(false);
+  const checkHanlder = () => {
+    setChecked((prevChecked) => {
+      const newChecked = !prevChecked;
+      checkItemsHandler(id, newChecked);
+      return newChecked;
+    });
+  };
+
   return (
     <View style={styles.manageCardView}>
       <View style={styles.manageCard}>
         <View style={{ justifyContent: "center", flex: 0.4 }}>
-          {isSelect ? (
-            <Ionicons name="checkmark-circle" size={24} color="black" />
-          ) : (
-            <Ionicons name="ellipse-outline" size={24} color="black" />
-          )}
+          <Pressable onPress={checkHanlder}>
+            {checked ? (
+              <Ionicons name="checkmark-circle" size={24} color="black" />
+            ) : (
+              <Ionicons name="ellipse-outline" size={24} color="black" />
+            )}
+          </Pressable>
         </View>
         <View style={{ flex: 1 }}>
           <Image style={styles.image} source={LionImg} contentFit="cover" />
@@ -100,22 +111,12 @@ export default function ManageCard() {
           <Text>온도 990 º</Text>
         </View>
         <View style={styles.buttonContainer}>
+          <View style={{ flex: 1 }}></View>
           <View style={{ flex: 1 }}>
             <TouchableOpacity>
               <Button
                 style={{ borderWidth: 0 }}
-                buttonColor={theme.psColor}
-                textColor="white"
-              >
-                수락
-              </Button>
-            </TouchableOpacity>
-          </View>
-          <View style={{ flex: 1 }}>
-            <TouchableOpacity>
-              <Button
-                style={{ borderWidth: 0 }}
-                buttonColor="#fa9797"
+                buttonColor={theme.btnRejectColor}
                 textColor="white"
               >
                 거절
@@ -128,7 +129,10 @@ export default function ManageCard() {
   );
 }
 const styles = StyleSheet.create({
-  manageCardView: { height: theme.screenHeight / 10, borderWidth: 1 },
+  manageCardView: {
+    height: theme.screenHeight / 10,
+    // borderWidth: 1
+  },
   manageCard: {
     flex: 1,
     alignItems: "center",
@@ -136,7 +140,7 @@ const styles = StyleSheet.create({
   },
   image: {
     borderWidth: 1,
-    borderColor: theme.profileBorderColor,
+    borderColor: "lightgray",
     width: 60,
     height: 60,
     borderRadius: theme.screenWidth / 6,
