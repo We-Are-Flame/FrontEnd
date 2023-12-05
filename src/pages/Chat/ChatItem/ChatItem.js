064b1b153a54becc63d89826d0abfaa2dd0b603d
pueropstar <majesoung@naver.com>
2023-12-05 19:29:45 +0900

    chore: prettier 적용

--- src/pages/Chat/ChatItem/ChatItem.js
+++ src/pages/Chat/ChatItem/ChatItem.js
@@ -1,55 +1,77 @@
-import React,{useEffect,useState} from "react";
-import { View, StyleSheet, Text,TouchableOpacity,Dimensions } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { Image } from "expo-image";
-import { Octicons } from '@expo/vector-icons';
+import { Octicons } from "@expo/vector-icons";
 
 import kitchingLogo from "../../../../assets/kitchingLogo.png";
-const screenWidth = Dimensions.get('window').width;
+const screenWidth = Dimensions.get("window").width;
 
 export default function ChatItem({ chatData }) {
   const navigation = useNavigation();
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const options = { hour: '2-digit', minute: '2-digit', hour12: true };
-    const formattedDate = new Intl.DateTimeFormat('ko-KR', options).format(date);
-  
+    const options = { hour: "2-digit", minute: "2-digit", hour12: true };
+    const formattedDate = new Intl.DateTimeFormat("ko-KR", options).format(
+      date
+    );
+
     // '오전/오후'를 'AM/PM' 대신 사용하기 위해 변환합니다.
-    return formattedDate.replace('AM', '오전').replace('PM', '오후');
+    return formattedDate.replace("AM", "오전").replace("PM", "오후");
   };
-  
+
   const DateTimeDisplay = ({ dateString }) => {
     const formattedTime = formatDate(dateString);
     return <Text>{formattedTime}</Text>;
   };
-  
+
   return (
-    <TouchableOpacity style={styles.chatItemView}
+    <TouchableOpacity
+      style={styles.chatItemView}
       onPress={() => {
-        navigation.navigate("ChatDetailPage",{roomId:chatData.room_id});
+        navigation.navigate("ChatDetailPage", { roomId: chatData.room_id });
       }}
     >
-      <View style={{flex:1}}>
+      <View style={{ flex: 1 }}>
         <Image
           source={chatData.thumbnail_url || kitchingLogo}
-          style={{ width: 30, height: 30, backgroundColor:"#eeeeee",padding:20, borderRadius:10 }}
+          style={{
+            width: 30,
+            height: 30,
+            backgroundColor: "#eeeeee",
+            padding: 20,
+            borderRadius: 10,
+          }}
           resizeMode="cover" // 또는 "contain", "stretch" 등
         />
       </View>
       <View style={styles.chatItemContent}>
         <View>
-          <Text style={{fontSize:16, fontWeight:"bold"}}>{chatData.room_name}</Text>
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+            {chatData.room_name}
+          </Text>
           <Text>{chatData.last_message}</Text>
         </View>
         <View>
-          <Text style={{color:"#aaaaaa"}}>
-            {
-              (chatData.is_notification) ? 
-              <Octicons name="bell" size={14} color="#aaaaaa" />:
+          <Text style={{ color: "#aaaaaa" }}>
+            {chatData.is_notification ? (
+              <Octicons name="bell" size={14} color="#aaaaaa" />
+            ) : (
               <Octicons name="bell-slash" size={14} color="#aaaaaa" />
-            }&nbsp;<DateTimeDisplay dateString={chatData.last_date_time}/></Text>
+            )}
+            &nbsp;
+            <DateTimeDisplay dateString={chatData.last_date_time} />
+          </Text>
         </View>
       </View>
     </TouchableOpacity>
@@ -58,15 +80,16 @@ export default function ChatItem({ chatData }) {
 
 const styles = StyleSheet.create({
   chatItemView: {
-    width:screenWidth,
-    flexDirection:"row",
-    paddingLeft:20,
-    flex:1,
+    paddingHorizontal: 20,
+    width: screenWidth,
+    flexDirection: "row",
+
+    flex: 1,
   },
-  chatItemContent:{
-    flexDirection:"row",
-    justifyContent:"space-between",
-    marginLeft:10,
-    flex:9,
-  }
+  chatItemContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginLeft: 10,
+    flex: 9,
+  },
 });
