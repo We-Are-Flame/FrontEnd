cdb94b6bb8ee3ae9b7e55a6459e748206244e8cc
pueropstar <majesoung@naver.com>
2023-12-05 19:29:27 +0900

    style: 대안 스타일 주석처리

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -1,21 +1,32 @@
-import Carousel from 'react-native-snap-carousel';
+/** @format */
+
+import Carousel from "react-native-snap-carousel";
 import { Image } from "expo-image";
-import {
-  View,
-  StyleSheet
-} from "react-native";
+import { View, StyleSheet } from "react-native";
 
 //<MyCarousel entries={carouselArr} widthProps={Dimensions.get('window').width} heightProps={200} layout="default"/>
-export default function MyCarousel({entries,widthProps,heightProps,layout}) {
-
-  const renderItem = ({item, index}) => {
+export default function MyCarousel({
+  entries,
+  widthProps,
+  heightProps,
+  layout,
+}) {
+  const renderItem = ({ item, index }) => {
     return (
-      <View style={[styles.imageView, {height:heightProps}]}>
+      <View style={[styles.imageView, { height: heightProps }]}>
         {/* 여기에 이미지나 다른 내용을 추가할 수 있습니다. */}
         <Image
           source={item}
-          style={{ width: widthProps-100, height: heightProps, borderRadius:10 }} // 예시 크기, 원하는 대로 조절
-          resizeMode="cover" // 또는 "contain", "stretch" 등
+          style={{
+            // width: "100%",
+            // height: undefined,
+            // aspectRatio: 1,
+            // borderRadius: 10,
+            width: widthProps - 100,
+            height: heightProps,
+          }} // 예시 크기, 원하는 대로 조절
+          // contentFit="scale-down" // 또는 "contain", "stretch" 등
+          contentFit="cover"
         />
       </View>
     );
@@ -26,16 +37,16 @@ export default function MyCarousel({entries,widthProps,heightProps,layout}) {
       data={entries}
       renderItem={renderItem}
       sliderWidth={widthProps}
-      itemWidth={widthProps-100}
+      itemWidth={widthProps - 100}
       layout={layout}
     />
   );
 }
 
 const styles = StyleSheet.create({
-  imageView:{
-    justifyContent:"center",
-    alignItems:"center",
-    borderRadius:10,
-  }
+  imageView: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
 });
