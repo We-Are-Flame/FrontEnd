52334f885af389d63ae03e7e3889d47d2552f67b
pueropstar <majesoung@naver.com>
2023-12-05 19:05:29 +0900

    feat: 데이터 가져오는 동안 로딩화면 추가

--- src/pages/Chat/ChatScreen.js
+++ src/pages/Chat/ChatScreen.js
@@ -1,56 +1,84 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Button, FlatList } from "react-native";
-import { TextInput } from "react-native-paper";
+/** @format */
+
+import { useEffect, useState, useCallback } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  FlatList,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+
 import Header from "./../../components/Header";
 import theme from "./../../styles/theme";
 
 import ChatItem from "./ChatItem/ChatItem";
 import axios from "axios";
 import userStore from "../../store/userStore";
-import { Image } from "expo-image";
 
-import Spinner from "../../../assets/loading_spinner.svg";
+import { API_URL } from "@env";
 
 export default function ChatScreen({ navigation }) {
+  const [refreshing, setRefreshing] = useState(false);
   const [data, setData] = useState([]);
-
+  const [pageLoading, setPageLoading] = useState(false);
   const { userToken } = userStore();
 
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+      fetchData();
+    }, 1000);
+  }, []);
+
+  const fetchData = async () => {
+    setPageLoading(true);
+    const res = await axios.get(`${API_URL}/api/chat/rooms`, {
+      headers: {
+        "Content-Type": `application/json`,
+        Authorization: "Bearer " + `${userToken}`,
+      },
+    });
+    setData(res.data.content);
+    console.log(res.data.content);
+    setPageLoading(false);
+  };
   useEffect(() => {
-    axios
-      .get(`http://118.67.128.48/api/chat/rooms`, {
-        headers: {
-          "Content-Type": `application/json`,
-          Authorization: "Bearer " + `${userToken}`,
-        },
-      })
-      .then((res) => {
-        console.log(res.data.content);
-        setData(res.data.content);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    fetchData();
   }, []);
 
   return (
     <View style={styles.chatScreenView}>
-      <View
-        style={{ flex: theme.headerSpace, backgroundColor: theme.psColor }}
-      ></View>
+      {pageLoading ? (
+        <View style={{ flex: 1, ...theme.centerStyle }}>
+          <ActivityIndicator size="large" color="black" />
+        </View>
+      ) : (
+        <View style={{ flex: 1 }}>
+          <View
+            style={{ flex: theme.headerSpace, backgroundColor: theme.psColor }}
+          ></View>
 
-      <Header />
-      <View style={{ flex: 7, backgroundColor: "#ffffff" }}>
-        <FlatList
-          style={{ marginTop: 40 }}
-          data={data}
-          renderItem={({ item, index }) => (
-            <ChatItem chatData={item} key={index} />
-          )}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
-        />
-      </View>
+          <Header />
+          <View style={{ flex: 7, backgroundColor: "#ffffff" }}>
+            <FlatList
+              refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+              }
+              style={{ marginTop: 40 }}
+              data={data}
+              renderItem={({ item, index }) => (
+                <ChatItem chatData={item} key={index} />
+              )}
+              keyExtractor={(item, index) => index.toString()}
+              ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+            />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
