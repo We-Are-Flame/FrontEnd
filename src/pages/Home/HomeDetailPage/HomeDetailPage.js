b2bc27f74739b7c8c37cc2aa11ef875bf5cb82ab
마재성 <majesoung@naver.com>
2023-12-05 22:39:13 +0900

    feat: 모임 탈퇴 시에 메인화면으로 이동하도록 변경

--- src/pages/Home/HomeDetailPage/HomeDetailPage.js
+++ src/pages/Home/HomeDetailPage/HomeDetailPage.js
@@ -199,10 +199,45 @@ export default function HomeDetailPage({ route }) {
             }
           );
           setIsUpdate(!isUpdate);
-          console.log(res.data);
+          //채팅방 입장 통신 추가할 것
         },
       },
     ]);
+  };
+
+  const clickCancel = () => {
+    Alert.alert(
+      "모임을 탈퇴하시겠습니까?",
+      `${detailData.info.title}을 탈퇴합니다.`,
+      [
+        {
+          text: "취소",
+          onPress: () => {
+            console.log("취소");
+          },
+        },
+        {
+          text: "확인",
+          onPress: () => {
+            axios
+              .post(`${API_URL}/api/meetings/${detailData.id}/cancel`, null, {
+                headers: {
+                  "Content-Type": `application/json`,
+                  Authorization: "Bearer " + `${userToken}`,
+                },
+              })
+              .then((res) => {
+                console.log(res.data);
+                setIsUpdate(!isUpdate);
+                //채팅방 퇴장 통신 추가할 것
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          },
+        },
+      ]
+    );
   };
 
   return Object.keys(detailData).length !== 0 ? (
@@ -398,7 +433,10 @@ export default function HomeDetailPage({ route }) {
                     </Text>
                   </TouchableOpacity>
                 ) : detailData.status.participate_status === "ACCEPTED" ? (
-                  <TouchableOpacity style={styles.homeDetailStateBtnRed}>
+                  <TouchableOpacity
+                    style={styles.homeDetailStateBtnRed}
+                    onPress={clickCancel}
+                  >
                     <Text style={styles.homeDetailStateTextRed}>참가 취소</Text>
                   </TouchableOpacity>
                 ) : detailData.status.participate_status === "PENDING" ? (
