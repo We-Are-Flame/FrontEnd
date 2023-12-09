96169b92397e21e1684776603617c2254c7d2adf
마재성 <majesoung@naver.com>
2023-12-10 01:15:59 +0900

    feat: 검색 구현중

---
+++ src/pages/Search/RecentlyItems.js
@@ -0,0 +1,56 @@
+/** @format */
+
+import { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import theme from "../../styles/theme";
+
+import { Feather, EvilIcons, AntDesign } from "@expo/vector-icons";
+
+import { API_URL } from "@env";
+import searchStore from "../../store/searchStore";
+export default function RecentlyItems({ data }) {
+  const { searchText, setSearchText } = searchStore();
+
+  //   useEffect(() => {
+  //     console.log(searchText);
+  //   }, [searchText]);
+
+  return (
+    <View style={styles.recentlyItemsView}>
+      <View style={styles.iconContainer}>
+        <Feather name="clock" size={16} color="gray" />
+      </View>
+      <View style={{ flex: 1, marginLeft: 10 }}>
+        <Text numberOfLines={1} style={{ fontSize: 18 }}>
+          {data.title}
+        </Text>
+      </View>
+      <View style={{ flexGrow: 0, alignItems: "flex-end" }}>
+        <AntDesign name="close" size={20} color="#868B94" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  recentlyItemsView: {
+    // backgroundColor: "red",
+
+    // padding: 20,
+    height: 60,
+    flexDirection: "row",
+
+    alignItems: "center",
+  },
+  iconContainer: {
+    ...theme.centerStyle,
+
+    width: 30,
+    height: 30,
+    flexGrow: 0,
+    borderWidth: 1,
+    borderColor: "lightgray",
+    borderRadius: 15,
+  },
+});
---
+++ src/pages/Search/RecentlySearch.js
@@ -0,0 +1,191 @@
+// /** @format */
+
+// import { useState, useEffect } from "react";
+// import { View, Text, StyleSheet } from "react-native";
+
+// import theme from "../../styles/theme";
+
+// import { Feather, EvilIcons, AntDesign } from "@expo/vector-icons";
+
+// import { API_URL } from "@env";
+// import searchStore from "../../store/searchStore";
+// import RecentlyItems from "./RecentlyItems";
+
+// export default function RecentlySearch({ data }) {
+//   const { searchText, setSearchText } = searchStore();
+
+//   useEffect(() => {
+//     console.log(searchText);
+//   }, [searchText]);
+
+//   return (
+//     <View style={{ flex: 1 }}>
+//       <View style={styles.contentHeader}>
+//         <Text style={{ fontWeight: "600", fontSize: 16 }}>최근 검색</Text>
+//         <Text style={{ fontSize: 16 }}>전체 삭제</Text>
+//       </View>
+
+//       <View style={styles.recentlyItems}>
+//         {data.content &&
+//           data.content.map((item, index) => {
+//             return <RecentlyItems item={item} key={index} />;
+//           })}
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   contentHeader: {
+//     flexGrow: 0,
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//   },
+//   recentlyItems: {
+//     flexGrow: 1,
+//   },
+//   randomScreenHeaderSpace: {
+//     flex: theme.headerSpace,
+//     backgroundColor: theme.psColor,
+//   },
+// });
+
+// /** @format */
+
+// import React, { useState, useEffect } from "react";
+// import {
+//   View,
+//   Text,
+//   Dimensions,
+//   StyleSheet,
+//   TextInput,
+//   ScrollView,
+// } from "react-native";
+
+// import theme from "../../styles/theme";
+// import axios from "axios";
+// import userStore from "../../store/userStore";
+// import { API_URL } from "@env";
+// import searchStore from "../../store/searchStore";
+// import RecentlySearch from "./RecentlySearch";
+// import { recentData } from "../../utils/StaticData";
+
+// export default function SearchContent() {
+//   const { searchText, setSearchText } = searchStore();
+//   const [datalength, setDataLenght] = useState(1);
+//   useEffect(() => {
+//     console.log(searchText);
+//   }, [searchText]);
+
+//   return (
+//     <View style={styles.searchContentView}>
+//       {recentData.count > 10 ? (
+//         <ScrollView style={{ flex: 1, padding: 20 }}>
+//           <RecentlySearch data={recentData} />
+//         </ScrollView>
+//       ) : (
+//         <View style={{ flex: 1, padding: 20 }}>
+//           <RecentlySearch data={recentData} />
+//         </View>
+//       )}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   searchContentView: { backgroundColor: "white", flex: 1 },
+
+//   contentHeader: {
+//     flexGrow: 0,
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//   },
+//   recentlyItems: {
+//     flexGrow: 1,
+//   },
+//   randomScreenHeaderSpace: {
+//     flex: theme.headerSpace,
+//     backgroundColor: theme.psColor,
+//   },
+// });
+// /** @format */
+
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+import theme from "../../styles/theme";
+import axios from "axios";
+import userStore from "../../store/userStore";
+import { API_URL } from "@env";
+import searchStore from "../../store/searchStore";
+import { recentData } from "../../utils/StaticData";
+import RecentlyItems from "./RecentlyItems";
+
+export default function RecentlySearch() {
+  const { searchText, setSearchText } = searchStore();
+  const [datalength, setDataLenght] = useState(1);
+  //   useEffect(() => {
+  //     console.log(searchText);
+  //   }, [searchText]);
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.searchContentView}>
+        {recentData.count > 10 ? (
+          <ScrollView style={{ flex: 1, padding: 20 }}>
+            <View style={styles.contentHeader}>
+              <Text style={{ fontWeight: "600", fontSize: 16 }}>최근 검색</Text>
+              <Text style={{ fontSize: 16 }}>전체 삭제</Text>
+            </View>
+
+            <View style={styles.recentlyItems}>
+              {recentData.content &&
+                recentData.content.map((data, index) => {
+                  console.log(data);
+                  return <RecentlyItems data={data} key={index} />;
+                })}
+            </View>
+          </ScrollView>
+        ) : (
+          <View style={{ flex: 1, padding: 20 }}>
+            <View style={styles.contentHeader}>
+              <Text style={{ fontWeight: "600", fontSize: 16 }}>최근 검색</Text>
+              <Text style={{ fontSize: 16 }}>전체 삭제</Text>
+            </View>
+
+            <View style={styles.recentlyItems}>
+              {recentData.content &&
+                recentData.content.map((data, index) => {
+                  console.log(data);
+                  return <RecentlyItems data={data} key={index} />;
+                })}
+            </View>
+          </View>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  searchContentView: { backgroundColor: "white", flex: 1 },
+  contentHeader: {
+    flexGrow: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  recentlyItems: {
+    flexGrow: 1,
+  },
+  randomScreenHeaderSpace: {
+    flex: theme.headerSpace,
+    backgroundColor: theme.psColor,
+  },
+});
