d11d32a7456b3c362e741e7506f5bcbd79d3f767
마재성 <majesoung@naver.com>
2023-12-11 00:27:44 +0900

    chore: 안쓰는 코드 제거

--- src/pages/Search/SearchContent.js
+++ src/pages/Search/SearchContent.js
@@ -8,22 +8,11 @@ import {
   ScrollView,
 } from "react-native";
 
-import theme from "../../styles/theme";
-import axios from "axios";
-import userStore from "../../store/userStore";
-import { API_URL } from "@env";
-import searchStore from "../../store/searchStore";
 import RecentlySearch from "./RecentlySearch";
-import { recentData } from "../../utils/StaticData";
 import SearchResult from "./SearchResult";
-
+import searchStore from "../../store/searchStore";
 export default function SearchContent() {
-  const { searchText, showResult } = searchStore();
-  const [datalength, setDataLenght] = useState(1);
-
-  //   useEffect(() => {
-  //     console.log(searchText);
-  //   }, [searchText]);
+  const { showResult } = searchStore();
 
   return (
     <View style={styles.searchContentView}>
